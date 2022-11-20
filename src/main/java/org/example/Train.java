2f5f0d8a4b788cd5d8ae5fe719b899ce767979b0
AlsuNuts <forever-nuts@yandex.ru>
2022-11-20 21:50:45 +0300

    Init commit

--- src/main/java/org/example/Train.java
+++ src/main/java/org/example/Train.java
@@ -1,14 +1,17 @@
 package org.example;
 
+import lombok.*;
+
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+
 public class Train extends Transport{
     private String id;
     private int carriageCount;
     private boolean isExpress;
-
-    public Train(String name, int capacity, int speed, float costOfKm) {
-        super(name, capacity, speed, costOfKm);
-    }
-
     @Override
     float getPrice(City city) {
         return getCostOfKm() * city.getDistanceKm();
